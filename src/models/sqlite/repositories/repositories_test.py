615525aecbf2c88abf5248f9d5e984a01155bffe
ezzcoutinho <lammerlopes@hotmail.com>
2025-03-11 20:58:54 -0300

    skip database interaction tests for PetsRepository and PeopleRepository

--- src/models/sqlite/repositories/repositories_test.py
+++ src/models/sqlite/repositories/repositories_test.py
@@ -1,14 +1,17 @@
+import pytest
 from src.models.sqlite.settings.connection import db_connection_handler
 from src.models.sqlite.repositories.pets_repository import PetsRepository
 from src.models.sqlite.repositories.people_repository import PeopleRepository
 
 db_connection_handler.connect_to_db()
 
+@pytest.mark.skip(reason="interacao com o banco")
 def test_list_pets():
   repo = PetsRepository(db_connection_handler)
   response = repo.list_pets()
   print(response)
 
+@pytest.mark.skip(reason="interacao com o banco")
 def test_list_people():
   repo = PeopleRepository(db_connection_handler)
   response = repo.list_people()
