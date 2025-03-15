ef9685639fa01a1c49b2e4cdb4e398b2e58c91ca
ezzcoutinho <lammerlopes@hotmail.com>
2025-03-14 22:06:37 -0300

    Updating with test integration

--- src/models/sqlite/repositories/repositories_test.py
+++ src/models/sqlite/repositories/repositories_test.py
@@ -12,12 +12,15 @@ def test_list_pets():
   print(response)
 
 @pytest.mark.skip(reason="interacao com o banco")
-def test_list_people():
-  repo = PeopleRepository(db_connection_handler)
-  response = repo.list_people()
-  print(response)
-
 def test_delete_pet():
   name = "belinha"
   repo = PetsRepository(db_connection_handler)
   repo.delete_pets(name)
+
+def test_insert_person():
+  first_name = "Teste"
+  last_name = "teste"
+  age = 20
+  pet_id = 1
+  repo = PeopleRepository(db_connection_handler)
+  repo.inser_person(first_name, last_name, age, pet_id)
