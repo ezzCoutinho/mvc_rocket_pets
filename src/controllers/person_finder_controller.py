e79cceaf233f37714bf6e3a87a0d759a5df4d91e
ezzcoutinho <lammerlopes@hotmail.com>
2025-03-17 20:59:05 -0300

    Adding; person tests of integration and fix Type for type.

--- src/controllers/person_finder_controller.py
+++ src/controllers/person_finder_controller.py
@@ -28,7 +28,7 @@ class PersonFinderController:
           "first_name": person.first_name,
           "last_name": person.last_name,
           "pet_name": person.pet_name,
-          "pet_type": person.pet_Type
+          "pet_type": person.pet_type
         }
       }
     }
