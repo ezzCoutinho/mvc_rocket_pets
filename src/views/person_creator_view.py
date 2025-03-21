64f702ab9671084dff8e22de45501cd01c017a6a
ezzcoutinho <lammerlopes@hotmail.com>
2025-03-20 21:09:27 -0300

    feat: add person creator view to handle person creation requests

---
+++ src/views/person_creator_view.py
@@ -0,0 +1,18 @@
+#pylint: disable=line-too-long
+from src.controllers.interfaces.person_creator_controller_interface import PersonCreatorControllerInterface
+from src.views.http_types.http_request import HttpRequest
+from src.views.http_types.http_response import HttpResponse
+from src.views.interfaces.view_interface import ViewInterface
+
+class PersonCreatorView(ViewInterface):
+  def __init__(self, controller: PersonCreatorControllerInterface) -> None:
+    self.__controller = controller
+
+  def handle(self, httpRequest: HttpRequest) -> HttpResponse:
+    person_info = httpRequest.body
+    body_response = self.__controller.create_person(person_info)
+
+    return HttpResponse(
+      status_code=201,
+      body=body_response
+    )
