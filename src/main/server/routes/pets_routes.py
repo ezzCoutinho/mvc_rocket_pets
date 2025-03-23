87f3376343d170eb6506ed962cda99f3a4db0547
ezzcoutinho <lammerlopes@hotmail.com>
2025-03-22 23:40:34 -0300

    feat(pets_routes): implement list and delete pet endpoints
    Add functionality to list all pets and delete a pet by name using the pet_list_composer and pet_deleter_composer. This change integrates the existing composers with the routes to handle HTTP requests and responses properly.

--- src/main/server/routes/pets_routes.py
+++ src/main/server/routes/pets_routes.py
@@ -1,7 +1,22 @@
 from flask import Blueprint, jsonify
+from src.views.http_types.http_request import HttpRequest
+from src.composer.pets_list_composer import pet_list_composer
+from src.composer.pets_deleter_composer import pet_deleter_composer
 
 pet_route_bp = Blueprint("pets_routes", __name__)
 
 @pet_route_bp.route("/pets", methods=["GET"])
 def list_pets():
-  return jsonify({"message": "list pets"}), 200
+  http_request = HttpRequest()
+  view = pet_list_composer()
+
+  http_response = view.handle(http_request)
+  return jsonify(http_response.body), http_response.status_code
+
+@pet_route_bp.route("/pets/<string:name>", methods=["DELETE"])
+def dete_pet(name: str):
+  http_request = HttpRequest(param={"name": name})
+  view = pet_deleter_composer()
+
+  http_response = view.handle(http_request)
+  return jsonify(http_response.body), http_response.status_code
