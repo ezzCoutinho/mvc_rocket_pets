4b396345a4c3657974991bad7d18c8eceba63156
ezzcoutinho <lammerlopes@hotmail.com>
2025-03-24 19:42:10 -0300

    feat(person_creator_view): add input validation before person creation
    Introduce validation using `person_creator_validator` to ensure the input data is valid before processing the person creation request. This improves data integrity and prevents potential errors.

--- src/views/person_creator_view.py
+++ src/views/person_creator_view.py
@@ -3,12 +3,15 @@ from src.controllers.interfaces.person_creator_controller_interface import Perso
 from src.views.http_types.http_request import HttpRequest
 from src.views.http_types.http_response import HttpResponse
 from src.views.interfaces.view_interface import ViewInterface
+from src.validators.person_creator_validator import person_creator_validator
 
 class PersonCreatorView(ViewInterface):
   def __init__(self, controller: PersonCreatorControllerInterface) -> None:
     self.__controller = controller
 
   def handle(self, http_request: HttpRequest) -> HttpResponse:
+    person_creator_validator(http_request)
+
     person_info = http_request.body
     body_response = self.__controller.create_person(person_info)
 
