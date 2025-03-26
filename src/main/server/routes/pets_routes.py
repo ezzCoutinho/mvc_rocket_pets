8beb020141e46cf8ed4d8cf3f0fb179620569561
ezzcoutinho <lammerlopes@hotmail.com>
2025-03-26 20:52:17 -0300

    fix(pets_routes): add error handling for pet deletion endpoint
    Introduce error handling in the pet deletion route to gracefully manage exceptions and return appropriate error responses. This ensures the API remains robust and provides meaningful feedback in case of failures.

--- src/main/server/routes/pets_routes.py
+++ src/main/server/routes/pets_routes.py
@@ -2,6 +2,8 @@ from flask import Blueprint, jsonify
 from src.views.http_types.http_request import HttpRequest
 from src.composer.pets_list_composer import pet_list_composer
 from src.composer.pets_deleter_composer import pet_deleter_composer
+from src.errors.error_types.error_controller import handle_errors
+
 
 pet_route_bp = Blueprint("pets_routes", __name__)
 
@@ -15,8 +17,12 @@ def list_pets():
 
 @pet_route_bp.route("/pets/<string:name>", methods=["DELETE"])
 def dete_pet(name: str):
-  http_request = HttpRequest(param={"name": name})
-  view = pet_deleter_composer()
+  try:
+    http_request = HttpRequest(param={"name": name})
+    view = pet_deleter_composer()
 
-  http_response = view.handle(http_request)
-  return jsonify(http_response.body), http_response.status_code
+    http_response = view.handle(http_request)
+    return jsonify(http_response.body), http_response.status_code
+  except Exception as exception:
+    response = handle_errors(exception)
+    return jsonify(response["body"]), response["status_code"]
